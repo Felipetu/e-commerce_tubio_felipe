3f5297387de7aa03517b4bf447da4b250cf21fb9
Feli <pepito@donkey.com>
2024-10-02 16:34:59 -0300

    variable etiquetas

--- producto.js
+++ producto.js
@@ -8,5 +8,12 @@ class producto{
     }
 }
 // ¿Como pongo una imagen ahi ._.? // No se si estara bien de esa forma...
-const mercaderia = new producto("Bugatti Chiron", "Es un automóvil superdeportivo diseñado y desarrollado por Bugatti Automobiles S.A.S", 3274362, 30, Img(src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/800px-FoS20162016_0624_132444AA_%2827785299372%29.jpg"))
+const mercaderia = new producto("Bugatti Chiron", "Es un automóvil superdeportivo diseñado y desarrollado por Bugatti Automobiles S.A.S", 3300000, 30, Image(src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/800px-FoS20162016_0624_132444AA_%2827785299372%29.jpg"))
 
+let etiquetas = 
+`<h1> titulo: ${producto.title}</h1>
+<h2> Descripcion: ${producto.detail}</h2>
+<h3> Precio: ${producto.price} </h3>
+<p> Stock: ${producto.stock} </p>
+${producto.image}
+`
