bc1604b7b62df0d8b1cb9a8ab28371b3ef876b72
Feli <pepito@donkey.com>
2024-10-02 17:31:36 -0300

    array mackaroo

--- index.js
+++ index.js
@@ -22,3 +22,68 @@ function tarjetas(){
 }
 
 tarjetas();
+
+[{
+  "id": 1,
+  "first_name": "Jodie",
+  "last_name": "Mumm",
+  "email": "jmumm0@gmpg.org",
+  "gender": "Female",
+  "ip_address": "40.129.201.83"
+}, {
+  "id": 2,
+  "first_name": "Del",
+  "last_name": "Hadgkiss",
+  "email": "dhadgkiss1@ucsd.edu",
+  "gender": "Female",
+  "ip_address": "59.113.141.28"
+}, {
+  "id": 3,
+  "first_name": "Kacie",
+  "last_name": "Stocks",
+  "email": "kstocks2@arstechnica.com",
+  "gender": "Genderfluid",
+  "ip_address": "204.234.60.20"
+}, {
+  "id": 4,
+  "first_name": "Herve",
+  "last_name": "Ferrarini",
+  "email": "hferrarini3@ucoz.com",
+  "gender": "Bigender",
+  "ip_address": "39.221.246.25"
+}, {
+  "id": 5,
+  "first_name": "Finn",
+  "last_name": "Kyrkeman",
+  "email": "fkyrkeman4@yahoo.com",
+  "gender": "Male",
+  "ip_address": "251.128.171.168"
+}, {
+  "id": 6,
+  "first_name": "Alissa",
+  "last_name": "Hoolaghan",
+  "email": "ahoolaghan5@tripod.com",
+  "gender": "Genderqueer",
+  "ip_address": "149.214.4.98"
+}, {
+  "id": 7,
+  "first_name": "Daron",
+  "last_name": "Petrasso",
+  "email": "dpetrasso6@shareasale.com",
+  "gender": "Male",
+  "ip_address": "130.25.60.232"
+}, {
+  "id": 8,
+  "first_name": "Alvy",
+  "last_name": "Seeviour",
+  "email": "aseeviour7@vinaora.com",
+  "gender": "Male",
+  "ip_address": "164.109.38.13"
+}, {
+  "id": 9,
+  "first_name": "Prudence",
+  "last_name": "Meece",
+  "email": "pmeece8@redcross.org",
+  "gender": "Non-binary",
+  "ip_address": "179.206.105.51"
+}]
--- navbar.js
+++ navbar.js
@@ -11,3 +11,4 @@ menu = `<ul>${menu}</ul>` //encapsular todo dentro de una lista <ul>
 const header = document.querySelector('header');
 
 header.innerHTML = menu;
+
