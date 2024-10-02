2b52a65e95345d0b6af606e006a1edc87aebacaf
Feli <pepito@donkey.com>
2024-10-02 16:44:29 -0300

    404

--- producto.js
+++ producto.js
@@ -11,7 +11,7 @@ class producto{
 const mercaderia = new producto("Bugatti Chiron", "Es un automóvil superdeportivo diseñado y desarrollado por Bugatti Automobiles S.A.S", 3300000, 30, Image(src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/800px-FoS20162016_0624_132444AA_%2827785299372%29.jpg"))
 
 let etiquetas = 
-`<h1> titulo: ${producto.title}</h1>
+`<h1> Titulo: ${producto.title}</h1>
 <h2> Descripcion: ${producto.detail}</h2>
 <h3> Precio: ${producto.price} </h3>
 <p> Stock: ${producto.stock} </p>
