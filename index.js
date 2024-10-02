2132dc0d120c52bb151c6a42b37052debded05d4
Feli <pepito@donkey.com>
2024-10-02 15:59:16 -0300

    function tarjeta

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-console.log("Hola");
-let h1 = document.querySelector(".productos");
-h1.innerHTML = "Productos";
+function tarjetas(){
+  const h1 = document.querySelector("h1");
 
-let section = document.querySelector(".container");
-
-let array = [];
-
-for (let i = 1; i <= 9; i ++) {
-    array.push(`
-<div class="card">
+  const section = document.querySelector("section")
+  
+  h1.innerText = "Productos";
+  
+  let array = [];
+  
+  for (let i = 1; i <= 9; i ++) { array.push(
+    `<div class="card">
   <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Auto">
   <div class="card-body">
     <h5 class="card-title">Card ${i}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary"> Mostrar</a>
   </div>
-</div>`);
-arraycard.push(card)
+  </div>`
+  );}
+  
+  section.innerHTML= array.join().replaceAll(",", ""); //union del array a string, y se eliminan las comas
 }
-section.innerHTML= array;
-document.querySelector("section")
+
+tarjetas();
