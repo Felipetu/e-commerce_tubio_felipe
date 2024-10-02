4d7e386993f9978f5263e51df55293007f576fad
Feli <pepito@donkey.com>
2024-10-02 16:20:13 -0300

    obj new producto

--- producto.js
+++ producto.js
@@ -7,3 +7,6 @@ class producto{
         this.image = image;
     }
 }
+// ¿Como pongo una imagen ahi ._.? // No se si estara bien de esa forma...
+const mercaderia = new producto("Bugatti Chiron", "Es un automóvil superdeportivo diseñado y desarrollado por Bugatti Automobiles S.A.S", 3274362, 30, Img(src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/FoS20162016_0624_132444AA_%2827785299372%29.jpg/800px-FoS20162016_0624_132444AA_%2827785299372%29.jpg"))
+
