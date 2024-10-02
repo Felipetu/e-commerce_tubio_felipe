9620d08365499423b6b83588a0d8828c6374c6c8
Feli <pepito@donkey.com>
2024-10-02 16:06:17 -0300

    class product

---
+++ producto.js
@@ -0,0 +1,9 @@
+class producto{
+    constructor(title, detail, price, stock, image){
+        this.title = title;
+        this.detail = detail;
+        this.price = price;
+        this.stock = stock;
+        this.image = image;
+    }
+}
