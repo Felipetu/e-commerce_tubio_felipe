b888a48355ec076d4e12612897314903fbc4c991
Feli <pepito@donkey.com>
2024-10-02 17:15:39 -0300

    variable menu

--- navbar.js
+++ navbar.js
@@ -1,1 +1,9 @@
-let categoria = [vehiculos, tecnologia, futurista]
+let categorias = ["vehiculos", "tecnologia", "futurista"]
+
+let menu = ""; // iniciar la variable que almacenara el html
+
+for (let category of categorias){
+menu += `<li>${category}</li>`; // añadir cada categoria dentro de una etiqueta <li>
+}
+
+menu = `<ul>${menu}</ul>` //encapsular todo dentro de una lista <ul>
